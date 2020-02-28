659596eae3ec5eca7572792a7a5961e844340085
Evelyn <evodell@pm.me>
2020-02-28 15:25:28 -0800

    Made buttons pretty.  Made drawing less opaque

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById(`canvas`);
 const resetBtn = document.getElementById(`reset-btn`);
+const setBtn = document.getElementById(`set-btn`);
+let pixelRes = 128;
 
 function clearCanvas() {
   while (canvas.lastElementChild) {
@@ -14,6 +16,8 @@ function createCanvas(pixelNum) {
   for (let i = 0; i < pixelNum**2; i++) {
     let pixel = document.createElement(`div`);
     pixel.classList.add(`pixels`);
+    pixel.style.backgroundColor = `#5c5c5c`;
+    pixel.style.opacity = 1;
     canvas.appendChild(pixel);
     pixel.addEventListener(`mouseover`, () => {
       changePixelColor(i)
@@ -22,19 +26,24 @@ function createCanvas(pixelNum) {
 }
 
 function changePixelColor(index) {
-  let pixel = document.querySelectorAll(`.pixels`);
-  pixel[index].style.backgroundColor = `black`;
+  let pixel = document.querySelectorAll(`.pixels`)[index];
+  let pixelOpacity = pixel.style.opacity - 0.33;
+  pixel.style.opacity = pixelOpacity;
+}
+
+function setPixelRes() {
+  let pixelNum = prompt(`How many pixels should there be on each side?`);
+  if (isNaN(pixelNum)) {
+    setPixels();
+  } else {
+    return pixelNum;
+  }
 }
 
 resetBtn.addEventListener(`click`, () => {
-  let pixelNum = prompt(`How many pixels should there be on each side?`);
-  if (isNaN(pixelNum)) pixelNum = prompt(`Please enter only numbers`);
+  let pixelNum = setPixelRes();
+  if (pixelNum < 1 || !pixelNum) return;
   createCanvas(pixelNum);
 })
 
-canvas.addEventListener(`click`, (e) => {
-  console.log(e);
-  console.log(e.target.dataset);
-})
-
-createCanvas(128);
+createCanvas(pixelRes);
